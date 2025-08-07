13178d11600d9cd1203412e4011ecb19f2879ba7
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-08-07 16:24:54 +0800

    feat: complete exercise 3

--- src/main/java/org/example/StringLab.java
+++ src/main/java/org/example/StringLab.java
@@ -9,8 +9,13 @@ public class StringLab {
         // System.out.println("The length of the string is: " + length);
 
         // // Prediction: 'l'
-        String message = "Keep learning!";
-        char character = message.charAt(5);
-        System.out.println("The character at index 5 is: " + character);
+        // String message = "Keep learning!";
+        // char character = message.charAt(5);
+        // System.out.println("The character at index 5 is: " + character);
+
+        // Prediction: "is"
+        String statement = "Java is powerful.";
+        String part = statement.substring(5, 7);
+        System.out.println("The extracted substring is: " + part);
     }
 }
