066cf6d26b0c06565a26ec7e9d4f9cebd96e1327
Godfrey Angelo Convento <godfrey-a.convento@gcash.com>
2025-08-07 16:20:27 +0800

    feat: complete exercise 1

---
+++ src/main/java/org/example/StringLab.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class StringLab {
+    public static void main(String[] args) {
+
+        // // Prediction: 14
+        // String greeting = "Hello, Cadets!";
+        // int length = greeting.length();
+        // System.out.println("The length of the string is: " + length);
+
+    }
+}
